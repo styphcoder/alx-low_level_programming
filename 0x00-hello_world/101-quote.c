33bb8f26a6c6fbb612e97a543c3edddd7e029c23
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-16 10:10:24 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	fput("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
