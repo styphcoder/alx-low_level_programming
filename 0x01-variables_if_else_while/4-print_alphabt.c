a574b81a9da208137366467ea5fd800f09114ed0
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-17 06:49:48 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,13 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z' && letter != 'e' && letter != 'q'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter == 'e' || letter == 'q')
+		continue;
+	else
 		putchar(letter);
-
+	}
 	putchar('\n');
 
 	return (0);
