db3659dd9bdaca9cdbeb81e73f566740127dc561
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-17 07:18:11 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{	putchar('0' + n);
+		putchar(", ")
+
+	}
+	putchar('\n');
+
+	return (0);
+}
