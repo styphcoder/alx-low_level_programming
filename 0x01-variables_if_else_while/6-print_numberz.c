41782bdb1810905acda7ebf9211b31989f5d0ea5
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-17 07:06:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar('0' + n);
+
+	putchar('\n');
+
+	return (0);
+}
