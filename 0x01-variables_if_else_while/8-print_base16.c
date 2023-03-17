6b354046de0eeeb3203f07522272d2ccc168b51c
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-17 07:12:12 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
