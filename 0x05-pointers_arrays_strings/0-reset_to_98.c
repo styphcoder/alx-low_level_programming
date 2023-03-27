caa512386c72e904942a25f42bb11ffad8c32401
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-27 08:25:32 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value of an int.
+ *@n : the integer
+ * return 0 : sucess
+ */
+void reset_to_98(int *n)
+{ *n = 98;
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+
+#ifndef MAIN_H
+#define MAINH
+
+void reset_to_98(int *n);
+
+#endif
