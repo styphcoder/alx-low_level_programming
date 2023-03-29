ce6a0fe80a9da05fade82df936a11680e40ee182
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-29 07:17:58 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+int _putchar(char);
+char *_strcat(char *dest, char *src);
+char *_strcat(char*, char*);
+char *_strncat(char*, char*, int);
+char *_strncpy(char*, char*, int);
+char *leet(char *);
+int _strcmp(char*, char*);
+void reverse_array(int*, int);
+char *string_toupper(char*);
+char *cap_string(char*);
+char *rot13(char*);
+#endif
