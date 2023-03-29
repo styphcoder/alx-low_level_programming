af16c125f591c3f1465e6f5492d8df84ccebe5cf
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-03-29 06:27:31 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen - return the length of a string.
+ * *_strcat - concatenates two strings.
  * @dest: first string.
  * @src: second string.
  * Return: concatenated string.
