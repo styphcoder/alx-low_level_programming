ed8257a0b6811c7b08e758a18e5239b4089aa142
styphcoder <elmahfoudi.moustafa@gmail.com>
2023-04-07 07:11:50 -0700

    create the static library

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: first string.
+ * @src: second string.
+ * Return: concatenated string.
+ */
+char *_strcat(char *dest, char *src)
+{int i = 0, j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
